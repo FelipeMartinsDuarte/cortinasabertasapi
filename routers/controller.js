d0f4ae513a3a0dc92731c0db2248ba9694a31ec7
felipe <felipemartinsduarte@outlook.com.br>
2021-12-19 20:43:59 -0300

    Refatorando-codigo

--- routers/controller.js
+++ routers/controller.js
@@ -7,7 +7,12 @@ exports.uploads = async (req, res, next) => {
 
   const files = req.files;
 
-  
+  if(!files){
+    const error = new Error('Por favor escolha arquivos');
+    error.httpStatusCode = 400;
+    return next(error)
+  }
+
   let imgArray = files.map((file) => {
     //Convertendo para 64 bits
 
@@ -29,26 +34,33 @@ exports.uploads = async (req, res, next) => {
         };
 
         //Após o Schema ser aplicado está salvando
-     let newImage = new imageModel(imagesSchema).save()
-     .then((savedDoc) => { 
+     let newImage = new imageModel(imagesSchema);
+     
+     return newImage
+     .save()
+     .then((savedDoc) => {
       console.log("Imagem enviada");
       console.log(savedDoc.id);
-     }).catch(error =>{
-       if(error){
-         if(error.name === "MongoError" || error.code === 11000){
-           return console.log("Arquivo duplicado");
-         }
-         return console.log("Algo deu errado,tente novamente")
-       }
-     }) 
-
-     
-        
+     //send savedDoc to mongoose database 
+     })
+     .catch((err) => {
+      if(err){
+        if(err.name === "MongoError" || err.code === 11000){
+        return Promise.reject({error: 'Arquivo duplicado, já existe um arquivo igual' });
+        } else {
+        return Promise.reject({error: "Algo deu errado, tente novamente"});
+        }
+      }
+    })
+   
       });
 
-      
-  res.json(imgArray); //como este item está na verdade no route isto aqui é o (res,req){res.send("issoaqui")}
-    
+  Promise.all(result).then(msg => {
+    res.redirect('/'); //como este item está na verdade no route isto aqui é o (res,req){res.send("issoaqui")}
+  }).catch(err => {
+    res.json(err);
+  })
+  
 };
 
 
