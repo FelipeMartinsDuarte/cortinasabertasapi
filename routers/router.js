7a707af6c883f97e3a3932c41f96bc8e520c03a5
felipe <felipemartinsduarte@outlook.com.br>
2021-12-20 17:18:09 -0300

    postforms

--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,7 @@
 const route = require("express").Router();
 const controller = require("./controller");
-const mongoose = require("mongoose")
-const teamoptions = require("../models/teamoptions")
+const mongoose = require("mongoose");
+const teamoptions = require("../models/teamoptions");
 const bodyParser = require("body-parser");
 const { none } = require("./multer");
 const multer = require("./multer");
@@ -11,27 +11,55 @@ route.use(bodyParser.urlencoded({ extended: true }));
 //Caso queira uma imagem mudarpara multer.single('images') obs:usaremos no logo
 route.post("/uploadmultiple", multer.array("images", 12), controller.uploads);
 
+/* teamoptions
+.find({}, function(err,doc){
+    console.log(doc);
+}).select('name')
+*/
+
 route.post("/uploadform", (req, res) => {
-    
+  
+  
+    teamoptions
+    .find()
+    .select("name")
+    .lean()
+    .exec(function (err, doc) {
+      var array = [];
+      var lenght = 0;
+      //Converting to json and javascript object
+      let stri = JSON.stringify(doc);
+      let parser = JSON.parse(stri);
+      //Counting parse object as array
+      for(var k in parser) if(parser.hasOwnProperty(k)){lenght++;}
+      //Catching all names
+      for(let i = 0; i < lenght; i++){
+          array.push(parser[i].name)
+      };
+    });
+
+
   var { teamlist, teamlist2, teamlist3 } = req.body;
   const permitedValues = ["Medicos", "Fonologos", "Enfermeiros", "Psicologos"];
 
-  if (teamlist === teamlist2 || teamlist2 === teamlist3 || teamlist === teamlist3) {
-    return res.status(409).json({Error: "Você selecionou itens iguais"});
+  if (
+    teamlist === teamlist2 ||
+    teamlist2 === teamlist3 ||
+    teamlist === teamlist3
+  ) {
+    return res.status(409).json({ Error: "Você selecionou itens iguais" });
   } else {
     let array = [];
     array.push(teamlist, teamlist2, teamlist3);
 
-    let allFounded = array.every(ai => permitedValues.includes(ai));
+    let allFounded = array.every((ai) => permitedValues.includes(ai));
 
-    if(!allFounded){
-        return res.status(400).json({Error: "Algum item é invalido"});
-    } else{
-        return res.status(200).json({msg: "Item enviado com sucesso"});
+    if (!allFounded) {
+      return res.status(400).json({ Error: "Algum item é invalido" });
+    } else {
+      return res.status(200).json({ msg: "Item enviado com sucesso" });
     }
-
-
   }
-})
+});
 
 module.exports = route;
