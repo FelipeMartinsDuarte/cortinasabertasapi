b4155d4f884aefb2c073646717286b38e607d673
felipe <felipemartinsduarte@outlook.com.br>
2021-12-20 20:12:41 -0300

    postforms

--- routers/imageadd.js
+++ routers/imageadd.js
@@ -4,11 +4,9 @@ const fs = require("fs"); //para ler o arquivo e converter
 const { none } = require("./multer");
 const multer = require("multer");
 const mongoose = require("mongoose");
-const teamoptions = require("../models/teamoptions");
 const bodyParser = require("body-parser");
 route.use(bodyParser.json()); // to support JSON bodies
 route.use(bodyParser.urlencoded({ extended: true }));
-
 
 //Uploading files
 exports.uploads = async (req, res, next) => {
@@ -71,71 +69,4 @@ exports.uploads = async (req, res, next) => {
     .catch((err) => {
       res.json(err);
     });
-};
-
-
-//Add item to team
-exports.addteamitem = (req, res) => {
-  teamoptions
-    .find()
-    .select("name")
-    .lean()
-    .exec(function (err, doc) {
-      catchvalue(doc);
-    });
-
-  async function catchvalue(doc) {
-    try{
-    var allowthisvalues = [];
-    var lenght = 0;
-    //Converting to json and javascript object
-    let stri = JSON.stringify(doc);
-    let parser = JSON.parse(stri);
-    //Counting parse object as array
-    for (var k in parser)
-      if (parser.hasOwnProperty(k)) {
-        lenght++;
-      }
-    //Catching all names
-    for (let i = 0; i < lenght; i++) {
-      allowthisvalues.push(parser[i].name);
-    }
-    await verificator(allowthisvalues);
-  }catch{
-    await res.status(500).json({ Error: "Não conseguimos achar os items" })
-  }
-  }
-
-  //Handling post values
-  var { teamlist, teamlist2, teamlist3 } = req.body;
-
-  async function verificator(values) {
-    try{
-    //Push itens
-    let array = [];
-    array.push(teamlist, teamlist2, teamlist3);
-    //White-list values
-    let permitedValues = values;
-
-    //Verify duplicate files
-    if (
-      teamlist === teamlist2 ||
-      teamlist2 === teamlist3 ||
-      teamlist === teamlist3
-    ) {
-      await res.status(409).json({ Error: "Você selecionou itens iguais" });
-    } else {
-      //Verify valid value
-      let allFounded = array.every((ai) => permitedValues.includes(ai));
-
-      if (!allFounded) {
-        await res.status(400).json({ Error: "Algum item é invalido" });
-      } else {
-        await res.status(200).json({ msg: "Item enviado com sucesso" });
-      }
-    }
-  }catch{
-    await res.status(500).json({ Error: "Erro inesperado, tente novamente" })
-  }
-  }
 };
--- routers/router.js
+++ routers/router.js
@@ -1,5 +1,6 @@
 const route = require("express").Router();
-const controller = require("./controller");
+const imgadd = require("./imageadd");
+const teamadd = require("./teamadd");
 const mongoose = require("mongoose");
 const teamoptions = require("../models/teamoptions");
 const bodyParser = require("body-parser");
@@ -9,8 +10,8 @@ route.use(bodyParser.json()); // to support JSON bodies
 route.use(bodyParser.urlencoded({ extended: true }));
 
 //Caso queira uma imagem mudarpara multer.single('images') obs:usaremos no logo
-route.post("/uploadmultiple", multer.array("images", 12), controller.uploads);
+route.post("/uploadmultiple", multer.array("images", 12), imgadd.uploads);
 
-route.post("/uploadform", controller.addteamitem);
+route.post("/uploadform", teamadd.addteamitem);
 
 module.exports = route;
