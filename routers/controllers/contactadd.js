7e3604745781506c0540330e7b73aaf07c1ded1d
felipe <felipemartinsduarte@outlook.com.br>
2021-12-21 14:19:18 -0300

    addcontact

--- routers/controllers/contactadd.js
+++ routers/controllers/contactadd.js
@@ -11,19 +11,19 @@ exports.contactingadd = (req,res) => {
 
     //Existence verifing
     if(tel1 || tel2 || timestart && timeend){
-        //if only cellphone exist
+        //if only one cellphone exist
         if(tel1){
             return res.status(200).json({ msg: "Enviado com sucesso" });
-        //if tel2 exist and tel1 no
+        //if tel2 exist and tel1 exist
         }else if(tel1 && tel2) {
-            //To Send as array
+            //To Send as array to mongoose
             var array = [];
             var openhours = [];
             array.push(tel1,tel2)
             openhours.push(timestart,timeend)
             return res.status(200).json({ msg: "Enviado com sucesso" });
             } else{
-                //Telephone receive and tel2 is deleted
+                //If only Telephone2 exist, Telephone receive values and tel2 is deleted
                 tel1 = tel2;
                 tel2 = "";
                 return res.status(200).json({ msg: "Enviado com sucesso" });
