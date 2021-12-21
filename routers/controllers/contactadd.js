f8335a1e4c4778e6eeeb9e7828f327fcb77293ca
felipe <felipemartinsduarte@outlook.com.br>
2021-12-21 14:17:14 -0300

    addcontact

--- routers/controllers/contactadd.js
+++ routers/controllers/contactadd.js
@@ -7,5 +7,31 @@ route.use(bodyParser.json()); // to support JSON bodies
 route.use(bodyParser.urlencoded({ extended: true }));
 
 exports.contactingadd = (req,res) => {
-    res.send("Felipe");
-}
+    var {tel1,tel2,timestart,timeend,website} = req.body;
+
+    //Existence verifing
+    if(tel1 || tel2 || timestart && timeend){
+        //if only cellphone exist
+        if(tel1){
+            return res.status(200).json({ msg: "Enviado com sucesso" });
+        //if tel2 exist and tel1 no
+        }else if(tel1 && tel2) {
+            //To Send as array
+            var array = [];
+            var openhours = [];
+            array.push(tel1,tel2)
+            openhours.push(timestart,timeend)
+            return res.status(200).json({ msg: "Enviado com sucesso" });
+            } else{
+                //Telephone receive and tel2 is deleted
+                tel1 = tel2;
+                tel2 = "";
+                return res.status(200).json({ msg: "Enviado com sucesso" });
+            }
+          //if there is value only in second telephone
+        } else{
+            return res.status(400).json({ Error: "Informe pelo menos um telefone e sobre o horario de visita" });
+        };
+
+    
+    }
