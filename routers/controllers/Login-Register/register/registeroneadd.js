c9464cfff6792545521cefab917640c17d8edbe8
felipe <felipemartinsduarte@outlook.com.br>
2021-12-22 11:01:42 -0300

    loginregister

--- routers/controllers/Login-Register/register/registeroneadd.js
+++ routers/controllers/Login-Register/register/registeroneadd.js
@@ -5,29 +5,12 @@ const bodyParser = require("body-parser");
 route.use(bodyParser.json()); // to support JSON bodies
 route.use(bodyParser.urlencoded({ extended: true }));
 
-exports.addregister = (req,res) => {
-   
-    var{name,email} = req.body;
-    var error = []; //Here will be the messages to display in frontend
+exports.addregister = (req, res) => {
+  var { name, email } = req.body;
 
-    //Checking if there is name
-    if(!name){
-        error.push({Error: "Insira um nome"})
-    } else{
-        //Checking if there is email
-        if(!email){
-        error.push({Error: "Insira um email"})
-        } else {
-            //Checking if there is any errors to redirect
-            if(error.length == 0){
+  if (!name || !email) {
+    return res.status(400).json({ Error: "Preencha os campos" });
+  }
 
-
-                
-                return res.redirect("/register");
-            } else {
-                //Throw error if something ha]en
-                return res.status(500).json({ Error: "Algo deu errado tente novamente" });
-            }
-        }
-    }
-}
+  res.redirect("/register");
+};
